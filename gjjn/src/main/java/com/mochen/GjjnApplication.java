0f533df7110f6430a48db71bbe412f12cf3a396c
wanghb <wanghb@feifanuniv.com>
2024-05-29 15:14:30 +0800

    commit

--- gjjn/src/main/java/com/mochen/GjjnApplication.java
+++ gjjn/src/main/java/com/mochen/GjjnApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @MapperScan("com.mochen.dao")
 @EnableAsync
 @EnableScheduling
+@EnableRedisHttpSession
 public class GjjnApplication {
 
 	public static void main(String[] args) {
--- gjjn/src/main/java/com/mochen/utils/RedisConfig.java
+++ gjjn/src/main/java/com/mochen/utils/RedisConfig.java
@@ -6,17 +6,24 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import com.alibaba.fastjson.parser.ParserConfig;
+import org.springframework.session.data.redis.config.annotation.SpringSessionRedisConnectionFactory;
 
 @Configuration
 @EnableCaching
@@ -43,5 +50,39 @@ public class RedisConfig extends CachingConfigurerSupport {
 
 		return RedisCacheManager.builder(redisConnectionFactory).initialCacheNames(cacheNames)
 				.withInitialCacheConfigurations(configMap).build();
+	}
+
+	@Value("${spring.redis.host}")
+	private String host;
+	@Value("${spring.redis.port}")
+	private int port;
+	@Value("${spring.redis.database}")
+	private int datatabase;
+	@Value("${spring.redis.session.host}")
+	private String sessionHost;
+	@Value("${spring.redis.session.port}")
+	private int sessionPort;
+	@Value("${spring.redis.session.database}")
+	private int sessionDatabase;
+
+	@Bean
+	@Primary
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(host);
+		redisStandaloneConfiguration.setPort(port);
+		redisStandaloneConfiguration.setDatabase(datatabase);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	@Bean
+	@SpringSessionRedisConnectionFactory
+	@Qualifier("sessionRedisConnectionFactory")
+	public RedisConnectionFactory sessionRedisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(sessionHost);
+		redisStandaloneConfiguration.setPort(sessionPort);
+		redisStandaloneConfiguration.setDatabase(sessionDatabase);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 }
